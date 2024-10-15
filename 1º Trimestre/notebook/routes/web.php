e67fe6b72adfe588aea8d54ac349b3442a1d88e0
SantiagoJTB <santitorresbravo@gmail.com>
2024-10-16 01:35:33 +0200

    feat: Añadiendo el editor

--- 1º Trimestre/notebook/routes/web.php
+++ 1º Trimestre/notebook/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EditorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/editorpost', [EditorController::class,"editorpost"]);
+Route::any('/editor', [EditorController::class,"editorget"]);
+
