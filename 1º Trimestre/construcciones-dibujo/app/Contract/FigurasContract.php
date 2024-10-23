49b599175cae91eca99e6915709968870f5935bd
SantiagoJTB <santitorresbravo@gmail.com>
2024-10-23 18:42:09 +0100

    feat: trabajo de clase; creacion de contracts

---
+++ 1º Trimestre/construcciones-dibujo/app/Contract/FigurasContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Contracts;
+
+final class FiguraContract {
+
+    public const TABLE_NAME = "figuras";
+    public const COL_ID = "id";
+    public const TIPO_IMAGE = "tipo_image";
+
+    private function __construct() {}
+}
